cadbdf4c6a305c0f9514b7a57f3bf0c54026accf
Thanhhaitran1203 <thanhhaitran1203@gmail.com>
2023-12-19 09:52:56 +0700

    first commit

---
+++ NextDayCalculator/src/NextDayCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class NextDayCalculator {
+    private Date time;
+    public String result;
+    public String nextDayCalculator(){
+        int day = time.getDay ();
+        int month = time.getMonth ();
+        int year = time.getYear ();
+        switch (day)
+        case 31:
+            break;
+        case 30:
+            break;
+        case 29:
+            break;
+        case 28:
+            break;
+
+    }
+}
